cc0b45177f1ae080b8abfdb36c50d5edef4bb943
K271K <achmadieiev@bk.ru>
2023-03-13 22:39:00 +0500

    Laba3 created List, Node, push_back  and getSize

--- Laba3/Laba3/Laba3.cpp
+++ Laba3/Laba3/Laba3.cpp
@@ -1,31 +1,113 @@
 ﻿#include <iostream>
 
-
-class List {
+template<typename T>
+struct Node {
 public:
-    List() {
-        printf("List()\n");
-    }
-    ~List() {
-        printf("~List()\n");
-    }
-private:
-
-    template<typename T>
-    struct Node {
-    public:
-
-        Node(T data = T(), Node pNext = nullptr) : data(data), pNext(pNext) {
-             printf("Node(T data, Node pNext)\n")
-        }
-
-        Node* pNext;
-        T data;
-    };
+	Node* pNext;
+	T data;
+	Node(T _data = T(), Node* _pNext = nullptr) : data(_data), pNext(_pNext) {
+		printf("Node(T _data, Node* _pNext)\n");
+	}
 };
 
-int main()
-{
-    std::cout << "Hello World!\n";
+template<typename T>
+class List {
+public:
+	List();
+	~List();
+	
+	void push_back(T data);
+	int GetSize() {return Size;}
+	T& operator[](const int index);
+private:
+
+	
+
+	int Size;
+	Node <T> *Head;
+};
+
+template<typename T>
+List<T>::List(): Size(0), Head(nullptr)
+{ 
+	printf("List()\n");
 }
+
+template<typename T>
+List<T>::~List()
+{
+
+}
+
+template<typename T>
+void List<T>::push_back(T data)
+{
+	if (Head == nullptr)
+	{
+		Head = new Node<T>(data);
+	}
+	else 
+	{
+		Node<T>* current = this->Head;
+
+		while (current->pNext != nullptr) 
+		{
+			current = current->pNext;
+		}
+
+		current->pNext = new Node<T>(data);
+	}
+
+	Size++;
+}
+
+template<typename T>
+T& List<T>::operator[](const int index)
+{
+	int counter = 0;
+	Node<T>* current = this->Head;
+	while (current != nullptr)
+	{
+		if (counter == index)
+		{
+			return current->data;
+		}
+		else
+		{
+			current = current->pNext;
+			counter++;
+		}
+	}
+}
+
+class Point {
+public:
+	Point(): x(0), y(0)
+	{
+		printf("Point()\n");
+	}
+	
+	int GetCordX() {
+		return x;
+	}
+private:
+	int x;
+	int y;
+};
+
+
+int main() {
+	
+	List<Point> pointList;
+	Point A;
+	Point B;
+	pointList.push_back(A);
+	pointList.push_back(B);
+
+	std::cout << pointList.GetSize() << "\n";
+	std::cout << pointList[1].GetCordX() << "\n";
+
+	return 0;
+}
+
 
