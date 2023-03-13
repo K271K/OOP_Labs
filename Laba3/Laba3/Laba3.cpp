732951096665c5660e61b8242f6f7d4a4fe19042
K271K <achmadieiev@bk.ru>
2023-03-13 20:50:07 +0500

    Laba3 started

---
+++ Laba3/Laba3/Laba3.cpp
@@ -0,0 +1,31 @@
+﻿#include <iostream>
+
+
+class List {
+public:
+    List() {
+        printf("List()\n");
+    }
+    ~List() {
+        printf("~List()\n");
+    }
+private:
+
+    template<typename T>
+    struct Node {
+    public:
+
+        Node(T data = T(), Node pNext = nullptr) : data(data), pNext(pNext) {
+             printf("Node(T data, Node pNext)\n")
+        }
+
+        Node* pNext;
+        T data;
+    };
+};
+
+int main()
+{
+    std::cout << "Hello World!\n";
+}
+
