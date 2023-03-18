a0ff56326a57ffc9223fa4ad96e5a17f36a45c96
K271K <achmadieiev@bk.ru>
2023-03-18 16:56:00 +0500

    Laba3 in progress. List ready, testing in progress

--- Laba3/Laba3/Laba3.cpp
+++ Laba3/Laba3/Laba3.cpp
@@ -6,30 +6,49 @@ struct Node {
 public:
 	Node* pNext; //указатель на следующий элемент в списке
 	T data; //данные
+	T* object;
 	Node() : data(T()), pNext(nullptr) {
 		printf("Node()\n");
 	}
 	Node(T _data) : data(_data), pNext(nullptr) {
 		printf("Node(T _data)\n");
 	}
+	~Node() {
+		printf("~Node()\n");
+	}
 };
 
+//Класс для односвязного списка
 template<typename T>
 class List {
 public:
+
 	List();
 	~List();
 	
+	//очистка списка
 	void clear();
+	//добавить в конец списка
 	void push_back(T data);
+	//добавить в начало списка
+	void push_front(T data);
+	//удалить элемент из начала списка
 	void pop_front();
+	//удалить элемент из конца списка
+	void pop_back();
+	//вставить элемент 
+	void insert(T data, int index);
+	//удалить элементы по индексу
+	void remove(int index);
+	//вернуть размер списка
 	int GetSize() {return Size;}
+	//перегрузка оператора квадратные скобки
 	T& operator[](const int index);
+	
 private:
 
-	
-
 	int Size;
+	//"Голова" списка ( первый элемент )
 	Node <T> *Head;
 };
 
@@ -42,8 +61,9 @@ List<T>::List(): Size(0), Head(nullptr)
 template<typename T>
 List<T>::~List()
 {
-	printf("~List()\n");
 	clear();
+	printf("~List()\n");
+	
 }
 
 template<typename T>
@@ -58,10 +78,12 @@ void List<T>::clear()
 template<typename T>
 void List<T>::push_back(T data)
 {
+	//если головы нет, значит список пуст и создается нода-голова, в которую будут помещены данные
 	if (Head == nullptr)
 	{
 		Head = new Node<T>(data);
 	}
+	//если голова есть: создаем временный указатель, чтобы пройтись по нодам и найти ноду, у которой указатель на след элемент равен nullptr
 	else 
 	{
 		Node<T>* current = this->Head;
@@ -74,21 +96,99 @@ void List<T>::push_back(T data)
 		current->pNext = new Node<T>(data);
 	}
 
+	//увеличиваем size после добавления элемента
+	Size++;
+}
+
+template<typename T>
+void List<T>::push_front(T data)
+{
+	Node<T>* NewNode = new Node<T>(data);
+	NewNode->pNext = Head;
+	Head = NewNode;
 	Size++;
 }
 
 template<typename T>
 void List<T>::pop_front()
 {
+	//создаем временный указатель, в который присваиваем указаетль head
 	Node<T>* temp = Head;
+	//следующий элемент после head, становится head
 	Head = Head->pNext;
+	//удаляем указатель на старый head
 	delete temp;
+	//уменьшаем size после удаления элемента
 	Size--;
+}
+
+template<typename T>
+void List<T>::pop_back()
+{
+	remove(Size - 1);
+}
+
+template<typename T>
+void List<T>::insert(T data, int index)
+{
+	//если индекс 0, добавляем объект в начало
+	if (index == 0)
+	{
+		push_front(data);
+	}
+	else
+	{
+		//указатель, в который будет помещен указатель на объект предыдищуий перед индексом по которому надо добавить объект
+		Node<T>* previous = this->Head;
+		
+		for (int i = 0; i < index - 1; i++)
+		{
+			previous = previous->pNext;
+		}
+
+		//Создаю нову ноду и передаю туда данные, указатель новый ноды = равен указатнлю previous(указывает на объект, который имеет "index+1" индекс
+		//У previous указатель pNext ставлю на новую созданную ноду
+		Node<T>* NewNode = new Node<T>(data);
+		NewNode->pNext = previous->pNext;
+		previous->pNext = NewNode;
+
+		Size++;
+
+	}
+}
+
+template<typename T>
+void List<T>::remove(int index)
+{
+	//если нужно удалить элемент по индексу ноль, вызываем фукнция pop_front()
+	if (index == 0)
+	{
+		pop_front();
+	}
+	else
+	{
+		//указатель, в который будет помещен указатель на объект по индексу [index-1]
+		Node<T>* previous = this->Head;
+
+		for (int i = 0; i < index - 1; i++)
+		{
+			previous = previous->pNext;
+		}
+
+		//Указатель ToDelete указывает на элемент который нужно удалить, то есть следующий после элемента [index-1]
+		Node<T>* ToDelete = previous->pNext;
+		//меняем указатель pNext у previous на pNext->ToDelete, после чего pNext будет указывать на [index+1] элемент
+		previous->pNext = ToDelete->pNext;
+
+		delete ToDelete;
+		Size--;
+	}
 }
 
 template<typename T>
 T& List<T>::operator[](const int index)
 {
+	//вводим счетчик и пока он не равен переданному индексу идем по нодам, когда счетчик равен индексу, возвращаем данные
 	int counter = 0;
 	Node<T>* current = this->Head;
 	while (current != nullptr)
@@ -105,24 +205,56 @@ T& List<T>::operator[](const int index)
 	}
 }
 
+//класс для проверки
 class Point {
 public:
 	Point(): x(0), y(0)
 	{
 		printf("Point()\n");
 	}
+	Point(int x, int y)
+	{
+		printf("Point(int x, int y)\n");
+		this->x = x;
+		this->y = y;
+	}
+	Point(const Point& T) {
+		printf("Point(const Point& T)\n");
+		this->x = T.x;
+		this->y = T.y;
+	}
+	~Point() {
+		printf("~Point(%d, %d)\n", x, y);
+	}
 	
 	int GetCordX() {
 		return x;
 	}
-private:
+protected:
 	int x;
 	int y;
 };
 
+class ColoredPoint : Point {
+public:
+
+protected:
+	int color;
+};
+
 
 int main() {
+	const int COUNT_POINTS = 10;
+	List<Point> points;
 
+	Point A;
+	points.push_back(A);
+	
+	for (int i = 0; i < COUNT_POINTS; i++)
+	{
+		Point B(i,i);
+		points.push_back(B);
+	}
 
 
 	return 0;
