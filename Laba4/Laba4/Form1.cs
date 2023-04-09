1d13035e6b0a97bbbc3c37c31e4840ff17517494
K271K <achmadieiev@bk.ru>
2023-04-09 15:26:20 +0500

    Laba4 in progress

--- Laba4/Laba4/Form1.cs
+++ Laba4/Laba4/Form1.cs
@@ -1,34 +1,35 @@
+using System.Drawing;
+
 namespace Laba4
 {
     public partial class Form1 : Form
     {
-        Graphics g;
+        public Graphics g;
         Pen pen;
         Brush brush;
-       
+
         public Form1()
         {
             InitializeComponent();
             g = panel1.CreateGraphics();
-            pen = new Pen(Color.Black, 5);
-            brush = new SolidBrush(Color.Gray);
-
         }
         class CCircle
         {
             public float x, y;
             public float radius = 50;
-
-            public CCircle(int x, int y)
+            Pen pen = new Pen(Color.Black, 10);
+            public CCircle(int _x, int _y, Graphics _g)
             {
-                this.x = x;
-                this.y = y;
+                this.x = _x;
+                this.y = _y;
+                _g.DrawEllipse(pen, x, y, radius, radius);
+                _g.FillEllipse(Brushes.DeepPink, x, y, radius, radius);
             }
         }
         private void panel1_MouseClick(object sender, MouseEventArgs e)
         {
-            CCircle kryg = new CCircle(e.X - 25, e.Y - 25);
-            g.DrawEllipse(pen, kryg.x, kryg.y, kryg.radius, kryg.radius);
+            CCircle kryg = new CCircle(e.X - 25, e.Y - 25, g);
         }
+
     }
 }
