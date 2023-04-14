40234e41094ec0479f3f5bb71c0e4a3c28d60a20
K271K <achmadieiev@bk.ru>
2023-04-14 20:09:02 +0500

    Laba4 done
    Сделал лабу 4, в понедельник пойду сдавать

--- Laba4/Laba4/Form1.Designer.cs
+++ Laba4/Laba4/Form1.Designer.cs
@@ -32,31 +32,100 @@ namespace Laba4
         private void InitializeComponent()
         {
             panel1 = new Panel();
+            testContainer = new ListBox();
+            InterSection = new CheckBox();
+            label1 = new Label();
+            ctrlSwtich = new CheckBox();
+            label2 = new Label();
             SuspendLayout();
             // 
             // panel1
             // 
-            panel1.Dock = DockStyle.Fill;
-            panel1.Location = new Point(0, 0);
+            panel1.BorderStyle = BorderStyle.FixedSingle;
+            panel1.Location = new Point(0, 44);
             panel1.Name = "panel1";
-            panel1.Size = new Size(1523, 630);
+            panel1.Size = new Size(1299, 586);
             panel1.TabIndex = 0;
             panel1.MouseClick += panel1_MouseClick;
+            // 
+            // testContainer
+            // 
+            testContainer.FormattingEnabled = true;
+            testContainer.ItemHeight = 15;
+            testContainer.Location = new Point(1319, 44);
+            testContainer.Name = "testContainer";
+            testContainer.Size = new Size(180, 124);
+            testContainer.TabIndex = 1;
+            // 
+            // InterSection
+            // 
+            InterSection.AutoSize = true;
+            InterSection.Location = new Point(155, 16);
+            InterSection.Name = "InterSection";
+            InterSection.Size = new Size(89, 19);
+            InterSection.TabIndex = 2;
+            InterSection.Text = "InterSection";
+            InterSection.UseVisualStyleBackColor = true;
+            InterSection.CheckedChanged += InterSection_CheckedChanged;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(1319, 191);
+            label1.Name = "label1";
+            label1.Size = new Size(38, 15);
+            label1.TabIndex = 3;
+            label1.Text = "label1";
+            // 
+            // ctrlSwtich
+            // 
+            ctrlSwtich.AutoSize = true;
+            ctrlSwtich.Location = new Point(30, 16);
+            ctrlSwtich.Name = "ctrlSwtich";
+            ctrlSwtich.Size = new Size(82, 19);
+            ctrlSwtich.TabIndex = 4;
+            ctrlSwtich.Text = "CTRL work";
+            ctrlSwtich.UseVisualStyleBackColor = true;
+            ctrlSwtich.CheckedChanged += checkBox1_CheckedChanged;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(1320, 216);
+            label2.Name = "label2";
+            label2.Size = new Size(38, 15);
+            label2.TabIndex = 5;
+            label2.Text = "label2";
             // 
             // Form1
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
+            BackColor = Color.White;
             ClientSize = new Size(1523, 630);
+            Controls.Add(label2);
+            Controls.Add(ctrlSwtich);
+            Controls.Add(label1);
+            Controls.Add(InterSection);
+            Controls.Add(testContainer);
             Controls.Add(panel1);
+            KeyPreview = true;
             Name = "Form1";
             Text = "Form1";
+            KeyDown += Form1_KeyDown;
+            KeyUp += Form1_KeyUp;
             ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
 
         private Panel panel1;
+        private ListBox testContainer;
+        private CheckBox InterSection;
+        private Label label1;
+        private CheckBox ctrlSwtich;
+        private Label label2;
     }
 
 
--- Laba4/Laba4/Form1.cs
+++ Laba4/Laba4/Form1.cs
@@ -1,13 +1,17 @@
-using System.Drawing;
+using System.Drawing;//Подключаем для рисования
+using System.Collections.Generic;//Подключаем для использования списка
+using System.DirectoryServices;
+using System.Diagnostics.Eventing.Reader;
 
 namespace Laba4
 {
     public partial class Form1 : Form
     {
         public Graphics g;
-        Pen pen;
-        Brush brush;
-
+        bool interSection = false;
+        bool ctrlWork = false;
+        bool ctrlPressed = false;
+        List<CCircle> Circles = new List<CCircle>();
         public Form1()
         {
             InitializeComponent();
@@ -15,21 +19,198 @@ namespace Laba4
         }
         class CCircle
         {
-            public float x, y;
-            public float radius = 50;
-            Pen pen = new Pen(Color.Black, 10);
-            public CCircle(int _x, int _y, Graphics _g)
+            public int x, y;
+            public int radius = 50;
+            public bool Select = true;
+            public CCircle(int _x, int _y)
             {
                 this.x = _x;
                 this.y = _y;
-                _g.DrawEllipse(pen, x, y, radius, radius);
-                _g.FillEllipse(Brushes.DeepPink, x, y, radius, radius);
             }
+            public bool isSelect()
+            {
+                return Select;
+            }
+            public void CircleSelect()
+            {
+                Select = true;
+            }
+            public void CircleDeSelect()
+            {
+                Select = false;
+            }
+            public bool checkHit(int ClickX, int ClickY)
+            {
+                if (Math.Pow((ClickX - x), 2) + Math.Pow((ClickY - y), 2) < Math.Pow((radius / 2), 2))
+                {
+                    this.CircleSelect();
+                    return true;
+                }
+                return false;
+            }
+            public void Draw(Graphics _g)
+            {
+                if (!isSelect())
+                {
+                    _g.DrawEllipse(Pens.Black, x - 25, y - 25, radius, radius);
+                    _g.FillEllipse(Brushes.Green, x - 25, y - 25, radius, radius);
+                }
+                else
+                {
+                    _g.DrawEllipse(Pens.Blue, x - 25, y - 25, radius, radius);
+                    _g.FillEllipse(Brushes.Yellow, x - 25, y - 25, radius, radius);
+                }
+            }
+            public void Easier(Graphics _g)
+            {
+                _g.DrawEllipse(Pens.White, x - 25, y - 25, radius, radius);
+                _g.FillEllipse(Brushes.White, x - 25, y - 25, radius, radius);
+            }
+
+
         }
         private void panel1_MouseClick(object sender, MouseEventArgs e)
         {
-            CCircle kryg = new CCircle(e.X - 25, e.Y - 25, g);
+            List<CCircle> tempList = new List<CCircle>();
+            bool flag = false;
+
+            foreach (CCircle c in Circles)
+            {
+                if (ctrlPressed && ctrlWork)
+                {
+
+                    if (!interSection)
+                    {
+                        if (c.checkHit(e.X, e.Y))
+                        {
+                            tempList.Add(c);
+                            flag = true;
+                            break;
+                        }
+                        else
+                        {
+                            c.Draw(g);
+                        }
+                    }
+
+                    else if (interSection)
+                    {
+                        c.checkHit(e.X, e.Y);
+                        if (c.isSelect())
+                        {
+                            tempList.Add(c);
+                            flag = true;
+                        }
+                        else
+                        {
+                            c.Draw(g);
+                        }
+                    }
+                }
+                else
+                {
+                    c.CircleDeSelect();
+                    c.checkHit(e.X, e.Y);
+                    if (c.isSelect())
+                    {
+                        tempList.Add(c);
+                        flag = true;
+                    }
+                    else
+                    {
+                        c.Draw(g);
+                    }
+                }
+            }
+            if (!flag)
+            {
+                CCircle newCircle = new CCircle(e.X, e.Y);
+                Circles.Add(newCircle);
+                testContainer.Items.Add(newCircle);
+                newCircle.Draw(g);
+            }
+            else
+            {
+                if (interSection)
+                {
+                    foreach (CCircle checkedCircle in tempList)
+                    {
+                        checkedCircle.Draw(g);
+                    }
+                }
+                else if (!interSection)
+                {
+                    if (!ctrlWork || !ctrlPressed)
+                    {
+                        foreach (CCircle checkedCircle in tempList)
+                        {
+                            checkedCircle.CircleDeSelect();
+                            checkedCircle.Draw(g);
+                        }
+                        tempList[tempList.Count - 1].CircleSelect();
+                        tempList[tempList.Count - 1].Draw(g);
+                    }
+                    else if (ctrlWork && ctrlPressed)
+                    {
+                        foreach (CCircle checkedCircle in tempList)
+                        {
+                            checkedCircle.Draw(g);
+                        }
+
+
+                    }
+                }
+            }
+            tempList.Clear();
         }
+        private void InterSection_CheckedChanged(object sender, EventArgs e)
+        {
+            interSection = !interSection;
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            label1.Text = e.KeyData.ToString();
+            if (e.KeyCode == Keys.ControlKey)
+            {
+                ctrlPressed = true;
+            }
+            if (e.KeyCode == Keys.Delete)
+            {
+                List<CCircle> deleteCircles = new List<CCircle>();
+
+                foreach (CCircle c in Circles)
+                {
+                    if (c.isSelect())
+                    {
+                        c.Easier(g);
+                        deleteCircles.Add(c);
+                    }
+                }
+                foreach (CCircle c in deleteCircles)
+                {
+                    Circles.Remove(c);
+                }
+                if (Circles.Count > 0)
+                {
+                    Circles[Circles.Count - 1].CircleSelect();
+                    Circles[Circles.Count - 1].Draw(g);
+                }
+            }
+
+        }
+
+        private void Form1_KeyUp(object sender, KeyEventArgs e)
+        {
+            ctrlPressed = false;
+            label1.Text = e.KeyData.ToString();
+        }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+            ctrlWork = !ctrlWork;
+        }
+
 
     }
 }
