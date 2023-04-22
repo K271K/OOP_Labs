13bda67f53fc4145a33306c840f5ea6ad3d2ed00
K271K <achmadieiev@bk.ru>
2023-04-22 18:19:52 +0500

    Laba4.2 is broken
    Пришлось пересоздать проект  с лабораторной работой 4.2

---
+++ Laba4.2/Laba4.2/Form1.cs
@@ -0,0 +1,14 @@
+using System.IO;
+
+namespace Laba4._2
+{
+    public partial class Form1 : Form
+    {
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+    }
+}
